33533366d994686569a3c554c0ac4e340e23830a
Galine <webmaster@cyberbotics.com>
2020-03-30 18:47:08 +0200

    Added public key getter

---
+++ httpdocs/key.php
@@ -0,0 +1,22 @@
+<?php
+
+function stripped_key($public_key) {
+  $stripped = "";
+  $header = strlen("-----BEGIN PUBLIC KEY-----\n");
+  $footer = strlen("-----END PUBLIC KEY-----");
+  $l = strlen($public_key) - $footer;
+  for($i = $header; $i < $l; $i += 65)
+    $stripped .= substr($public_key, $i, 64);
+  $stripped = substr($stripped, 0, -2 - $footer);
+  return $stripped;
+}
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+$public_key_file = fopen("../id_rsa.pub", "r") or die("{\"error\":\"unable to open public key file\"}");
+$k = fread($public_key_file, filesize("../id_rsa.pub"));
+fclose($public_key_file);
+$key = stripped_key($k);
+die("{\"key\":\"$key\"}");
+?>
