9fcf5d984c8cbd2613529e79cfd0da1c743d6dc1
Galine <webmaster@cyberbotics.com>
2020-04-01 14:05:17 +0200

    fix

--- httpdocs/register.php
+++ httpdocs/register.php
@@ -1,6 +1,6 @@
 <?php
 function error($message) {
-  die("{\"error\":$message}");
+  die("{\"error\":\"$message\"}");
 }
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
@@ -10,14 +10,15 @@ if (!$publication)
   error("Unable to parse JSON post");
 if (!isset($publication->schema))
   error("Unable to read schema field");
-if (substr($publication->schema, 41, $length) !== 'https://directdemocracy.vote/json-schema/')
-  error("Wrong schema URL");
+$url = 'https://directdemocracy.vote/json-schema/';
+if (substr($publication->schema, 0, 41) !== $url)
+  error("Wrong schema URL: " . substr($publication->schema, 0, 41));
 $n = strpos($publication->schema, '/', 41);
 if (substr($publication->schema, $n) != '/ballot.schema.json')
   error("Wrong schema object");
 $directdemocracy_version = substr($publication->schema, 41, $n);
 if ($directdemocracy_version !== '0.0.1')
-  error("Unsupported version: " + $directdemocracy_version);
+  error("Unsupported version: $directdemocracy_version");
 
 die("{\"status\":\"success\"}");
 ?>
