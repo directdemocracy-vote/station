028e008f415d1a5b4322bb07e91ad70ef383f3c0
Galine <webmaster@cyberbotics.com>
2020-04-02 11:14:43 +0200

    test

--- httpdocs/register.php
+++ httpdocs/register.php
@@ -78,9 +78,9 @@ $success = openssl_sign($data, $signature, $private_key, OPENSSL_ALGO_SHA256);
 openssl_free_key($private_key);
 if ($success === FALSE)
   error("Failed to sign ballot.");
-$publication->station->signature = $signature;
+$publication->station->signature = base64_encode($signature);
 
 $data = json_encode($publication, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-die("{\"ballot\":\"$signature\"}");
+die("{\"ballot\":$data, \"signature\":\"" . base64_encode($signature) . "\"}");
 
 ?>
