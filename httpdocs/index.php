0c9cb60534fa2bff9db182bad6262dd6c1fbd198
Galine <webmaster@cyberbotics.com>
2020-04-10 22:56:37 +0200

    fixed

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -10,7 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="https://directdemocracy.vote/js/crypto-js.js"></script>
-    <title>observer.directdemocracy.vote</title>
+    <title>station.directdemocracy.vote</title>
   </head>
   <body>
     <div class='corner-ribbon' title="This web site is in beta quality: it may have bugs and change without notice. Please, report any problem to info@<?=$base_domain?>.">Beta</div>
