6e57ff3a5080b3c6cad89a6f3044af26b49fcc34
Galine <webmaster@cyberbotics.com>
2020-04-10 22:52:07 +0200

    updated

---
+++ httpdocs/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="https://directdemocracy.vote/favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://directdemocracy.vote/css/directdemocracy.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://directdemocracy.vote/js/crypto-js.js"></script>
+    <title>observer.directdemocracy.vote</title>
+  </head>
+  <body>
+    <div class='corner-ribbon' title="This web site is in beta quality: it may have bugs and change without notice. Please, report any problem to info@<?=$base_domain?>.">Beta</div>
+    <main role='main'>
+      <div class="jumbotron directdemocracy-title">
+        <div class="container">
+          <div class="row" style="margin-top:30px;margin-bottom:30px">
+            <div class="col-sd-1" style="margin-right:20px;margin-top:10px"><img class="directdemocracy-title-logo" src="//directdemocracy.vote/images/directdemocracy-title.png"></div>
+            <div class="col-sd-11">
+              <h1><b>direct</b>democracy</h1>
+              <div style="font-size:150%">polling station</div>
+            </div>
+          </div>
+          <div class="directdemocracy-subtitle" style="position:relative;top:0;margin-bottom:40px">
+            <h3>This webservice provides a polling station</h3>
+            <h3>allowing citizens to vote in directdemocracy.</h3>
+            <h3>Processes and results are auditable by anyone.</h3>
+          </div>
+        </div>
+        <br>
+      </div>
+    </main>
+    <div>
+      <hr>
+      <footer>
+        <p style='text-align:center'><small>Made by citizens for citizens</small></p>
+      </footer>
+    </div>
+  </body>
+</html>
