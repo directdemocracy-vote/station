27406d91739403ea728f5f8a46b899f17782e02a
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-01 15:10:10 +0100

    nothing

--- database/station.sql
+++ database/station.sql
@@ -3,16 +3,24 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `participation` (
+CREATE TABLE `vote` (
   `id` int(11) NOT NULL,
-  `referendum` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `publicKey` varchar(512) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `privateKey` varchar(2048) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `published` bigint(15) NOT NULL
+  `version` int(11) NOT NULL,
+  `key` blob NOT NULL,
+  `signature` blob NOT NULL,
+  `published` datetime NOT NULL,
+  `appKey` blob NOT NULL,
+  `appSignature` blob NOT NULL,
+  `referendum` blob NOT NULL,
+  `number` int(11) NOT NULL,
+  `ballot` blob NOT NULL,
+  `answer` text COLLATE utf8mb4_unicode_ci NOT NULL,
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 ALTER TABLE `participation`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `referendum` (`referendum`),
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `participation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 COMMIT;
