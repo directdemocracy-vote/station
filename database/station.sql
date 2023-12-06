9b2c6ca2f4b51278aae186c0d3974ab1cf143709
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-06 18:21:16 +0100

    Update station.sql

--- database/station.sql
+++ database/station.sql
@@ -13,6 +13,14 @@ CREATE TABLE `vote` (
   `answer` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `referendum` (
+  `signature` blob NOT NULL,
+  `deadline` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+ALTER TABLE `referendum`
+  ADD UNIQUE KEY `signature` (`signature`) USING HASH;
+
 ALTER TABLE `vote`
   ADD PRIMARY KEY (`id`);
   ADD UNIQUE KEY `referendum` (`referendum`,`ballot`) USING HASH;
