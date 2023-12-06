51664bdff95f24f898b7dc3bf6cddf84a9e07251
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-06 17:41:11 +0100

    Create publish_vote.php

---
+++ httpdocs/api/publish_vote.php
@@ -0,0 +1,46 @@
+<?php
+require_once(__DIR__.'/../../php/database.php');
+
+function error($message) {
+  die("{\"error\":\"$message\"}");
+}
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: content-type");
+
+$notary = 'https://notary.directdemocracy.vote';
+
+$query = "SELECT DISTINCT referendum FROM vote";
+$result = $mysqli->query($query) or die($mysqli->error);
+$list = '';
+while ($row = $result->fetch_assoc()) {
+  $participation = array(
+    'schema' => "https://directdemocracy.vote/json-schema/$row[version]/participation.schema.json",
+    'key' => $row['key'],
+    'signature' => $row['signature'],
+    'published' => intval($row['published']),
+    'appKey' => $row['appKey'],
+    'appSignature' => $row['appSignature'],
+    'referendum' => $row['referendum'],
+    'encryptedVote' => $row['encryptedVote']
+  );
+  $list .= $row['id'].', ';
+  $data = json_encode($participation, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+  $options = array('http' => array('method' => 'POST',
+                                   'content' => $data,
+                                   'ignore_errors' => true,
+                                   'header' => "Content-Type: application/json\r\n"
+                                              ."Accept: application/json\r\n"));
+  $response = file_get_contents("$notary/api/publish.php", false, stream_context_create($options));
+  $json = json_decode($response);
+  if (json_last_error() !== JSON_ERROR_NONE)
+    error($response);
+  if (isset($json->error))
+    error($json->error);
+  $mysqli->query("DELETE FROM participation WHERE id=$row[id]");
+}
+$result->free();
+$list = substr($list, 0, -2);
+die("{\"published\": \"$list\"}");
+?>
