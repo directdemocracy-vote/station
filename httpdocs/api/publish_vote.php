625e70930ce0b3575217591f1eeb00e498ab8969
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-07 09:29:56 +0100

    Update publish_vote.php

--- httpdocs/api/publish_vote.php
+++ httpdocs/api/publish_vote.php
@@ -20,7 +20,7 @@ $private_key = openssl_get_privatekey("file://".__DIR__."../../id_rsa");
 if ($private_key == FALSE)
   error("failed to read private key");
 
-$query = "SELECT REPLACE(REPLACE(TO_BASE64(signature), '\\n', ''), '=', ''), UNIX_TIMESTAMP(deadline) FROM referendum";
+$query = "SELECT REPLACE(REPLACE(TO_BASE64(signature), '\\n', ''), '=', ''), UNIX_TIMESTAMP(deadline) FROM referendum WHERE deadline <= NOW()";
 $result = $mysqli->query($query) or die($mysqli->error);
 $output = [];
 while ($row = $result->fetch_assoc()) {
