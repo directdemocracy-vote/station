194bac2d727464d274718ad913fde7f8a628c189
Olivier Michel <Olivier.Michel@cyberbotics.com>
2023-12-07 09:24:06 +0100

    Update publish_vote.php

--- httpdocs/api/publish_vote.php
+++ httpdocs/api/publish_vote.php
@@ -9,38 +9,63 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: content-type");
 
+$version = "2";
 $notary = 'https://notary.directdemocracy.vote';
 
-$query = "SELECT DISTINCT referendum FROM vote";
+$public_key_file = fopen(__DIR__."/../../id_rsa.pub", "r") or error("unable to open public key file");
+$k = fread($public_key_file, filesize("../../id_rsa.pub"));
+fclose($public_key_file);
+$key = stripped_key($k);
+$private_key = openssl_get_privatekey("file://".__DIR__."../../id_rsa");
+if ($private_key == FALSE)
+  error("failed to read private key");
+
+$query = "SELECT REPLACE(REPLACE(TO_BASE64(signature), '\\n', ''), '=', ''), UNIX_TIMESTAMP(deadline) FROM referendum WHERE deadline <= NOW()";
 $result = $mysqli->query($query) or die($mysqli->error);
-$list = '';
+$output = [];
 while ($row = $result->fetch_assoc()) {
-  $participation = array(
-    'schema' => "https://directdemocracy.vote/json-schema/$row[version]/participation.schema.json",
-    'key' => $row['key'],
-    'signature' => $row['signature'],
-    'published' => intval($row['published']),
-    'appKey' => $row['appKey'],
-    'appSignature' => $row['appSignature'],
-    'referendum' => $row['referendum'],
-    'encryptedVote' => $row['encryptedVote']
-  );
-  $list .= $row['id'].', ';
-  $data = json_encode($participation, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-  $options = array('http' => array('method' => 'POST',
-                                   'content' => $data,
-                                   'ignore_errors' => true,
-                                   'header' => "Content-Type: application/json\r\n"
-                                              ."Accept: application/json\r\n"));
-  $response = file_get_contents("$notary/api/publish.php", false, stream_context_create($options));
-  $json = json_decode($response);
-  if (json_last_error() !== JSON_ERROR_NONE)
-    error($response);
-  if (isset($json->error))
-    error($json->error);
-  $mysqli->query("DELETE FROM participation WHERE id=$row[id]");
+  $referendum = $row['signature'];
+  $deadline = intval($row['deadline']);
+  $output += [$referendum => 0];
+  $query = "SELECT id, appKey, appSignature, number, ballot, answer FROM vote WHERE referendum=TO_BASE64('$referendum==')";
+  $r = $mysqli->query($query) or error($mysqli->error);
+  while($vote = $r->fetch_assoc()) {
+    $vote = array(
+      'schema' => "https://directdemocracy.vote/json-schema/$version/participation.schema.json",
+      'key' => $key,
+      'signature' => '',
+      'published' => $deadline,
+      'appKey' => $vote['appKey'],
+      'appSignature' => $vote['appSignature'],
+      'referendum' => $vote['referendum'],
+      'number' => $vote['number'],
+      'ballot' => $vote['ballot'],
+      'answer' => $vote['answer']
+    );
+    $data = json_encode($vote, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    $signature = '';
+    $success = openssl_sign($data, $signature, $private_key, OPENSSL_ALGO_SHA256);
+    if ($success === FALSE)
+      error("failed to sign vote");
+    $vote['signature'] = substr(base64_encode($signature), 0, -2);
+    $data = json_encode($vote, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+    $options = array('http' => array('method' => 'POST',
+                                     'content' => $data,
+                                     'header' => "Content-Type: application/json\r\n" .
+                                                 "Accept: application/json\r\n"));
+    $response = file_get_contents("$notary/api/publish.php", false, stream_context_create($options));
+    $json = json_decode($response);
+    if (json_last_error() !== JSON_ERROR_NONE)
+      error($response);
+    if (isset($json->error))
+      error($json->error);
+    $mysqli->query("DELETE FROM vote WHERE id=$r[id]") or error($mysqli->error);
+    $output[$referendum]++;
+  }
+  $r->free();
+  $mysqli->query("DELETE FROM referendum WHERE signature=FROM_BASE64('$referendum==')") or error($mysqli->error);
 }
 $result->free();
-$list = substr($list, 0, -2);
+$list = json_encode($output, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 die("{\"published\": \"$list\"}");
 ?>
