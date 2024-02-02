4b63137586f0cab7d462260e87d6bdb1dab4ea34
Olivier Michel <Olivier.Michel@cyberbotics.com>
2024-02-02 17:08:04 +0100

    Update station.sql

--- database/station.sql
+++ database/station.sql
@@ -9,9 +9,9 @@ CREATE TABLE `vote` (
   `appSignature` blob NOT NULL,
   `referendum` blob NOT NULL,
   `number` int(11) NOT NULL,
+  `area` int(11) NOT NULL,
   `ballot` binary(32) NOT NULL,
-  `answer` text NOT NULL,
-  `area` int(11) NOT NULL
+  `answer` text NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `referendum` (
