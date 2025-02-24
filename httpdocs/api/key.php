c8aebc3f900b09b38398fb30f18da0a443b67d63
Olivier Michel <Olivier.Michel@cyberbotics.com>
2025-02-24 21:12:50 +0100

    Update key.php

--- httpdocs/api/key.php
+++ httpdocs/api/key.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../../php/header.php';
+
 function stripped_key($public_key) {
   $stripped = str_replace("-----BEGIN PUBLIC KEY-----", "", $public_key);
   $stripped = str_replace("-----END PUBLIC KEY-----", "", $stripped);
@@ -6,9 +8,6 @@ function stripped_key($public_key) {
   $stripped = str_replace("\n", '', $stripped);
   return $stripped;
 }
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: content-type");
 $public_key_file = fopen("../../id_rsa.pub", "r") or die("{\"error\":\"unable to open public key file\"}");
 $k = fread($public_key_file, filesize("../../id_rsa.pub"));
 fclose($public_key_file);
